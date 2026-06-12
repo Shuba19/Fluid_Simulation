8a04c4b29c28f75cb45c9e5b44c2551e2fd33e11
cegliotto <a.ceglia03@gmail.com>
2026-06-12 19:46:04 +0200

    mac sync

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -52,6 +52,9 @@ void createGraphicsPipeline(appState& state){
     vengono compilati alla creazione della graphic pipeline 
     quindi appena termina la creazione della pipeline possono essere distrutti 
     */
+    // auto vertShaderCode = readFile("../../src/shaders/vert.spv");
+    // auto fragShaderCode = readFile("../../src/shaders/frag.spv");
+
     auto vertShaderCode = readFile("../src/shaders/vert.spv");
     auto fragShaderCode = readFile("../src/shaders/frag.spv");
 
